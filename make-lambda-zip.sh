c067e25cf82f25772b8d0aaf324ac362535f9043
Kenshi Muto <kmuto@kmuto.jp>
2023-09-09 20:23:46 +0900

    rename

--- make-lambda-zip.sh
+++ make-lambda-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 bundle config set --local path 'vendor/bundle'
-rm -rf vendor mackerel-webhook-gateway-googlechat.zip
+rm -rf vendor saba-webhook-gateway-googlechat.zip
 bundle install --gemfile Gemfile-lambda
 mv vendor/bundle/ruby/3.1.0 vendor/bundle/ruby/3.2.0
 bundle config unset --local path
-zip -r mackerel-webhook-gateway-googlechat.zip exe/lambda-handler-googlechat.rb customize.rb lib vendor
+zip -r saba-webhook-gateway-googlechat.zip exe/lambda-handler-googlechat.rb customize.rb lib vendor
